21ee5a5656a66ef05966d42d7d0b644d2fb338a5
Juan <l21590492@sjuanrio.tecnm.mx>
2026-03-09 17:22:49 -0600

    1er Commit - 21590492

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hola Mundo"
+
+if __name__ == '__main__':
+    app.run(debug=True)
